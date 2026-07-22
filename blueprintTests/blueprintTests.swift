aad92d8c9f5f792cfe634ba9d03956677db69333
Luiz Mello <contato@luizmello.dev>
2026-07-22 11:42:07 -0300

    Initial project setup

---
+++ blueprintTests/blueprintTests.swift
@@ -0,0 +1,17 @@
+//
+//  blueprintTests.swift
+//  blueprintTests
+//
+//  Created by Luiz Mello on 22/07/26.
+//
+
+import Testing
+@testable import blueprint
+
+struct blueprintTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
